09a394f78ec515943ad950fa503c7c4c584c0084
kanyutu707 <kanyutualfred707@gmail.com>
2025-09-09 23:29:51 +0300

    readme edit

---
+++ build/CMakeFiles/packet_sniffer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packet_sniffer.dir/src/JsonExporter.cpp.o"
+  "CMakeFiles/packet_sniffer.dir/src/JsonExporter.cpp.o.d"
+  "CMakeFiles/packet_sniffer.dir/src/PacketSniffer.cpp.o"
+  "CMakeFiles/packet_sniffer.dir/src/PacketSniffer.cpp.o.d"
+  "CMakeFiles/packet_sniffer.dir/src/main.cpp.o"
+  "CMakeFiles/packet_sniffer.dir/src/main.cpp.o.d"
+  "packet_sniffer"
+  "packet_sniffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/packet_sniffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
